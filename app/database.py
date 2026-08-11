e18cc6e1c1b79fdab709ba8c0fb9adc7df6c2eb6
gautamshubhang <shubhang.gautam123@gmail.com>
2026-08-12 00:13:12 +0530

    postgres fixes

--- app/database.py
+++ app/database.py
@@ -1,17 +1,34 @@
-# from app.schemas import NoteResponse
-
-# shubhang1234
-# note_db = {
-#     1: NoteResponse(id=1, title="Note 1", content="This is the first note", created_at=datetime.now(), updated_at=datetime.now()),
-#     2: NoteResponse(id=2, title="Note 2", content="This is the second note", created_at=datetime.now(), updated_at=datetime.now())
-# }
-
-
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, URL
+import urllib.parse
 
+class DatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: int
+    db_name: str
 
-engine = create_engine("postgresql://postgres:shubhang@1234@localhost:5432/family_chat", echo=True)
+db_settings = DatabaseSettings()
+
+safe_password = urllib.parse.quote_plus(db_settings.db_password)
+
+print(f"--> APP TRYING USER: '{db_settings.db_user}'")
+print(f"--> APP TRYING PASS: '{db_settings.db_password}'") 
+
+db_url = URL.create(
+    drivername="postgresql",
+    username=db_settings.db_user,
+    password=safe_password, 
+    host=db_settings.db_host,
+    port=db_settings.db_port,
+    database=db_settings.db_name
+)
+
+engine = create_engine(db_url, echo=True)
 session = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
--- app/routers/notes.py
+++ app/routers/notes.py
@@ -8,25 +8,25 @@ from app.services.note_service import get_all_notes,get_note_by_id,create_new_no
 router = APIRouter(prefix="/notes", tags=["Notes"])
 
 
-router.get("/", response_model = list[NoteResponse])
+@router.get("/", response_model = list[NoteResponse])
 def get_all_notes_endpoint(db : Session = Depends(get_db)):
     note = get_all_notes(db)
     if not note:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No notes available")
     return note
 
-router.get("/{note_id}", response_model = NoteResponse)
+@router.get("/{note_id}", response_model = NoteResponse)
 def get_note_by_id_endpoint(note_id: int, db: Session = Depends(get_db)):
     note = get_note_by_id(note_id, db)
     if not note:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
     return note
 
-router.post("/", response_model = NoteResponse)
+@router.post("/", response_model = NoteResponse)
 def create_new_note_endpoint(note_data: NoteCreate, db: Session = Depends(get_db)):
     return create_new_note(note_data, db)
 
-router.put("/{note_id}", response_model = NoteResponse)
+@router.put("/{note_id}", response_model = NoteResponse)
 def update_note_endpoint(note_id: int, note: NoteUpdate, db: Session = Depends(get_db)):
     note_db_response = update_note(note_id, note, db)
     if note.title is None and note.content is None:
@@ -35,7 +35,7 @@ def update_note_endpoint(note_id: int, note: NoteUpdate, db: Session = Depends(g
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
     return note_db_response
 
-router.delete("/{note_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{note_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_note_endpoint(note_id: int, db: Session = Depends(get_db)):
     note_db_response = delete_note(note_id, db)
     if not note_db_response:
